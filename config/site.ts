1fb5e889e7c14f655812650236d64d29d8214dfa
Prachit <ingleprachit91@gmail.com>
2023-09-10 15:59:06 +0530

    hh

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name:"TryME",
+  description:"Expertly designed goods for the work space are here",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
